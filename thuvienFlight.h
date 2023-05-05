dc33907581970380b7172abc6148d1c335c30e1a
Phạm Thanh Thu <125842522+nobithu@users.noreply.github.com>
2023-05-05 08:41:17 +0700

    quan ly chyen bay

--- thuvienFlight.h
+++ thuvienFlight.h
@@ -48,13 +48,10 @@ bool DeleteAfter(FlightList &FL, FlightNode *TempoFlight ); // xoa mot cbay sau 
 bool DeleteFollowIDFlight(FlightList &FL, const char *MaChuyenBayCanXoa); // xoa theo id
 bool ClearFlightList(FlightList &FL); // xoa bo nho 
 void StatusFlight(FlightList &FL); // tu dong cap nhat trang thai cbay
-void TicketChairBoard(Flight F); // hien thi bang ghe ve cua 1 cbay
-//void EffectTicketMenu (int index,int PreColor)// hieuung menu 
+void TicketChairBoard(Flight F); // hien thi bang ghe ve cua 1 cbay 
 int ChooseTicket(Flight &F ); // chon 1 ve tu cbay
 void ShowFlight(Flight F, int position ); // show 1cb
 void ShowFlightList(FlightList FL ); // show ds cbay
-void ShowFlightListPerPage(FlightList FL, int StartIndex); // show theo trang
-void ChangePageManageFlightList(FlightList FL ); // doi trang khi quanly cbay
 bool FlightDataIsEmpty(Flight F); // ktra dulieu cua chuyen bay rong or not
 void InputFlight(FlightList &FL,bool EditedOrNot = false, bool DeleteOrNot=false); // nhap info cho 1 bay + them vao ds , xoa or sua maybay hien co
 void MenuManageFlightList(FlightList &FL); // show menu quan ly
